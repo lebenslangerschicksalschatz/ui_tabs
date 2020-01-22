54420f8795afe31fa88b5401f2b26cc20a0382a5
kusokkulochok@gmail.com <kusokkulochok@gmail.com>
2020-01-23 00:33:12 +0200

    animated circles

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       if (this.hash !== "") {
         event.preventDefault();
 
-        var hash = this.hash;
+        let hash = this.hash;
 
         $('html, body').animate({
           scrollTop: $(hash).offset().top
@@ -16,7 +16,7 @@ $(document).ready(function() {
   });
   //TABS
   $('.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+		let tab_id = $(this).attr('data-tab');
 
 		$('.tabs li').removeClass('active');
 		$('.tabs__content').removeClass('current');
@@ -27,7 +27,7 @@ $(document).ready(function() {
   //RATINGS
   /* 1. Visualizing things on Hover - See next part for action on click */
   $('#stars li').on('mouseover', function(){
-    var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+    let onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
    
   // Now highlight all the stars that's not after the current hovered star
   $(this).parent().children('li.star').each(function(e){
@@ -47,8 +47,8 @@ $(document).ready(function() {
 
   /* 2. Action to perform on click */
   $('#stars li').on('click', function(){
-    var onStar = parseInt($(this).data('value'), 10); // The star currently selected
-    var stars = $(this).parent().children('li.star');
+    let onStar = parseInt($(this).data('value'), 10); // The star currently selected
+    let stars = $(this).parent().children('li.star');
     
     for (i = 0; i < stars.length; i++) {
       $(stars[i]).removeClass('selected');
